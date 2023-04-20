edc84cca31d9cc7f806226215ebb98aa3393461a
ashleystefanyy <131294507+ashleystefanyy@users.noreply.github.com>
2023-04-20 14:47:52 -0300

    na aula

--- main.c
+++ main.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
+int main() {
+	  int cod;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+   printf( "\nDigite código entre 1 a 15:");
+	scanf( "%i", &cod );
 
-int main() 
-{
-	int n1, n2 , prod, dif, rest;
-    float quo;
-    printf( "Digite 2 numeros:");
-    printf("\n");
-   scanf( "%i", &n1);
-   scanf( "%i", &n2);
-	printf("\n Leu %i e %i", n1, n2);
-   prod =  n1 * n2;
-   printf("\n Produto= %i", prod);
-	dif = n2 - n1 ;
-	printf("\nDiferenca= %i", dif );
-	if (n2 != 0){
-		quo = (float) n1/ (float) n2 ;
-		printf("\nQuociente= %.2f", quo);
-		rest = n1 % n2 ;
-		printf("\nResto=%i", rest);
+    switch(cod){
+    	case 1:
+    		printf ("\nNão perecível");
+    		//break;
+    	case 2 ... 4:
+    		printf ("\nPerecivel");
+    		//break;
+    	case 5:
+    	case 6:
+    		printf ("\nProduto de higiene");
+    		//break;
+    	case 7:
+    		printf ("\nLimpeza");
+    		//break;
+    	case 8 ... 15:
+    		printf ("\nVestuario");
+    		//break;
+    	default:
+			printf ("\nCodigo invalido"); 
 	}
-	else    { 
-	printf ("\nCom denominador zero, nao pode calcular quociente e resto.");
-      
-	}
-     
 	return 0;
 }
